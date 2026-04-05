0217a5614e4dfa3d92dd0b1307c854e65e9771f3
Stuf_y <158745985+StuffzEZ@users.noreply.github.com>
2026-04-05 17:50:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -118,7 +118,7 @@ client.on(Events.MessageCreate, async (message) => {
 
   try {
     const response = await axios.get(webhookUrl, {
-      timeout: 65000,
+      timeout: 75000,
       auth: {
         username: WEBHOOK_USER,
         password: WEBHOOK_PASS,
